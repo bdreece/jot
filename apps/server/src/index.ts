655d5b459e7bc10e646ceab05064ad16c214c630
Brian Reece <bdreece@mtu.edu>
2022-11-18 21:34:59 -0500

    Updated @jot/server
    Updated package.json files

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -15,21 +15,48 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+import 'reflect-metadata';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import compression from 'compression';
 import morgan from 'morgan';
 
-import { ping } from './api';
+import { buildTypeDefsAndResolvers } from 'type-graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { graphqlHTTP } from 'express-graphql';
+import {
+  AssetCrudResolver,
+  DirectoryCrudResolver,
+  NoteCrudResolver,
+} from '@jot/data/generated/type-graphql/index';
+
+import * as api from './api';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(morgan('dev'));
+app.use(compression());
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors({ origin: '*' }));
-app.use(compression());
-app.use(morgan('dev'));
 
-app.get('/api/ping', ping.get);
+buildTypeDefsAndResolvers({
+  resolvers: [AssetCrudResolver, DirectoryCrudResolver, NoteCrudResolver],
+}).then(({ typeDefs, resolvers }) => {
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  app.use(
+    '/api/graphql',
+    graphqlHTTP({
+      schema,
+    }),
+  );
+});
+
+app.post('/api/auth/login', api.auth.login.post);
+app.post('/api/auth/register', api.auth.register.post);
+app.get('/api/auth/logout', api.auth.logout.get);
+app.get('/api/auth/refresh', api.auth.refresh.get);
 
 app.listen(PORT, () => console.log(`Listening on :${PORT}`));
